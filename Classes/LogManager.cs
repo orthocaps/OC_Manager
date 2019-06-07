fb72d75cb724dc059ccb0c55f8dd584e2d6140ff
jbenedix <julianbenedix@web.de>
2019-06-07 09:41:23 +0200

    Add files via upload

---
+++ Classes/LogManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ocManager
+{
+    public static class LogManager
+    {
+        public static List<string> LogFile = new List<string>();
+        public static void addToLog(string text)
+        {
+            LogFile.Add(DateTime.Now + " >> " + text);
+        }
+
+        public static void WriteLogFile()
+        {
+            File.WriteAllLines(ContentManager.LogFilePath, LogFile);
+        }
+
+        public static void loadLogFromFile()
+        {
+            LogFile = File.ReadAllLines(ContentManager.LogFilePath, Encoding.Default).ToList();
+        }
+    }
+}
